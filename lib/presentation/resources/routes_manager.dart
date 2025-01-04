468c8b8cd29f3c35a4190603e9a3a7d52c16bf74
pc <abdo.bissari@gmail.com>
2025-01-04 21:30:51 +0100

    Implemented routes and views, restructuring resources.

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced/presentation/login/login.dart';
+import 'package:flutter_advanced/presentation/main/main_view.dart';
+import 'package:flutter_advanced/presentation/register/register.dart';
+import 'package:flutter_advanced/presentation/splash/splash.dart';
+import 'package:flutter_advanced/presentation/store_details/store_details.dart';
+
+import '../forgot_password/forgot_password.dart';
+import '../onboarding/onboarding.dart';
+
 class Routes{
   static const String splashRoute = "/";
   static const String onBoardingRoute = "/onBoarding";
@@ -6,4 +16,40 @@ class Routes{
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator{
+  static Route<dynamic> getRoute(RouteSettings routeSettings){
+    switch(routeSettings.name){
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_)=> const SplashView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_)=> const OnBoardingView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_)=> const Login());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_)=> const Register());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_)=> const ForgotPassword());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_)=> const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_)=> const StoreDetails());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_)=> Scaffold(
+        appBar: AppBar(
+          title: const Text("No Route Found"),
+        ),
+        body: const Center(
+          child: Text("No Route Found"),
+        ),
+      ),
+    );
+  }
 }
