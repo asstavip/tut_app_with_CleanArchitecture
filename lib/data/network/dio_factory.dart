98447f167395ad415b6c01a423f2473b694ddf5e
pc <abdo.bissari@gmail.com>
2025-01-14 18:43:34 +0100

    feat: Add pretty dio logger for debug mode, and disable it for release mode

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../../app/constant.dart';
 
@@ -18,13 +20,25 @@ class DioFactory {
       AUTHORIZATION: Constant.token,
       DEFAULT_LANGUAGE: "en" // TODO: get language from settings
     };
+
     dio.options = BaseOptions(
       baseUrl: Constant.baseUrl,
       connectTimeout: Duration(minutes: _timeout),
       receiveTimeout: Duration(minutes: _timeout),
       headers: headers,
     );
+
     dio.options.baseUrl = Constant.baseUrl;
+    if(kReleaseMode){
+      print("Release Mode");
+    }else
+    {
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        responseBody: true,
+        responseHeader: true,
+      ));
+    }
     return dio;
   }
 }
