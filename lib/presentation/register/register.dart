3fe29cda26f15ba39e07288e8b77918a035723f2
asstavip <76185168+asstavip@users.noreply.github.com>
2025-01-29 18:42:33 +0100

    feat: Implement Register screen with state management
    - Implements a basic Register screen layout.
    - Integrates StateRenderer for handling different UI states.
    - Sets up the Register view model for state management.
    - Configures AppBar styling and system overlay for the Register screen.
    - Adds primary white color as background color.

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_advanced/presentation/common/state_renderer/state_renderer_impl.dart';
 import 'package:flutter_advanced/presentation/register/register_view_model.dart';
+import 'package:flutter_advanced/presentation/resources/color_pallete.dart';
 
 import '../../app/di.dart';
+import '../resources/values_manager.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -38,7 +42,30 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorPallete.primaryWhite,
+      appBar: AppBar(
+        elevation: AppSize.s0,
+        backgroundColor: ColorPallete.primaryWhite,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorPallete.primaryWhite,
+          statusBarIconBrightness: Brightness.dark,
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: StreamBuilder(
+        stream: _viewModel.outputState,
+        builder: (context, snapshot) {
+          return snapshot.data?.getScreenWidget(
+                  context, _getContentWidget(), _viewModel.register()) ??
+              _getContentWidget();
+        },
+      ),
+    );
+  }
+
+  Widget _getContentWidget() {
+    return Container();
   }
 
   @override
