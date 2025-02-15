ed9b84f2f7ae53ca61ba330f880befd4ac6ba3e4
asstavip <76185168+asstavip@users.noreply.github.com>
2025-02-15 23:14:41 +0100

    feat: Implement getHomeData API and repository (main)
    This commit implements the `getHomeData` API endpoint and
    repository method. It includes adding the API call to the
    `AppServiceClient`, implementing the repository method in
    `RepositoryImpl`, and adding the necessary data classes
    and mappings. The `HomeResponse` now extends from
    `BaseResponse`.
    
    This commit also adds the `HomeViewModel` and related
    stream controllers and refactors page file structure.

--- lib/presentation/main/pages/home/viewmodel/home_view_model.dart
+++ lib/presentation/main/pages/home/viewmodel/home_view_model.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:flutter_advanced/domain/model/model.dart';
 import 'package:flutter_advanced/domain/usecase/home_usecase.dart';
@@ -7,20 +6,21 @@ import 'package:rxdart/rxdart.dart';
 
 import '../../../../base/base_view_model.dart';
 
-class HomeViewModel extends BaseViewModel{
+class HomeViewModel extends BaseViewModel
+    implements HomeViewModelInput, HomeViewModelOutput {
+  final StreamController _bannerStreamController =
+      BehaviorSubject<List<BannerAd>>();
+  final StreamController _storeStreamController =
+      BehaviorSubject<List<Store>>();
+  final StreamController _serviceStreamController =
+      BehaviorSubject<List<Service>>();
 
-  final StreamController _bannerStreamController = BehaviorSubject<List<BannerAd>>();
-  final StreamController _storeStreamController = BehaviorSubject<List<Store>>();
-  final StreamController _serviceStreamController = BehaviorSubject<List<Service>>();
-
-  final HomeUseCase _homeUseCase ;
+  final HomeUseCase _homeUseCase;
 
   HomeViewModel(this._homeUseCase);
 
   @override
-  void start() {
-
-  }
+  void start() {}
 
   @override
   void dispose() {
@@ -29,4 +29,47 @@ class HomeViewModel extends BaseViewModel{
     _serviceStreamController.close();
     super.dispose();
   }
+
+  // * inputs *
+
+  @override
+  // TODO: implement inputBannerAd
+  Sink get inputBannerAd => _bannerStreamController.sink;
+
+  @override
+  // TODO: implement inputService
+  Sink get inputService => _serviceStreamController.sink;
+
+  @override
+  // TODO: implement inputStore
+  Sink get inputStore => _storeStreamController.sink;
+
+  // * outputs *
+  @override
+  // TODO: implement outputBannerAd
+  Stream<List<BannerAd>> get outputBannerAd => _bannerStreamController.stream.map((banner) => banner);
+
+  @override
+  // TODO: implement outputService
+  Stream<List<Service>> get outputService => _serviceStreamController.stream.map((service) => service);
+
+  @override
+  // TODO: implement outputStore
+  Stream<List<Store>> get outputStore => _storeStreamController.stream.map((store) => store);
+}
+
+abstract class HomeViewModelInput {
+  Sink get inputBannerAd;
+
+  Sink get inputStore;
+
+  Sink get inputService;
+}
+
+abstract class HomeViewModelOutput {
+  Stream<List<BannerAd>> get outputBannerAd;
+
+  Stream<List<Store>> get outputStore;
+
+  Stream<List<Service>> get outputService;
 }
